3b2facdbbccbe0cc2698d2699e0b984e10fa3fec
João Lucas <joao.silva@gfinnovation.com.br>
2025-07-26 17:15:28 -0300

    [ADD]: adicionado classe para formatar o erro

---
+++ src/main/java/io/github/jsousa32/libdealsign/utils/ErrorUtils.java
@@ -0,0 +1,21 @@
+package io.github.jsousa32.libdealsign.utils;
+
+import java.util.Set;
+
+public final class ErrorUtils {
+
+    private ErrorUtils() {
+    }
+
+    public static void checkIfHasAnyError(final Set<String> errors) {
+        if (errors.isEmpty()) return;
+
+        final var errorsInString = String.join(", ", errors);
+
+        final var initialPhrase = errors.size() == 1 ? "O atributo " : "Os atributos ";
+
+        final var finalPhrase = errors.size() == 1 ? " é obrigatório." : " são obrigatórios.";
+
+        throw new RuntimeException(initialPhrase.concat(errorsInString).concat(finalPhrase));
+    }
+}
