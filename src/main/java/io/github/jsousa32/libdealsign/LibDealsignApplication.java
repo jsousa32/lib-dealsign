520bb39fd936db22adcb6c754762de45555857e8
João Lucas <joao.silva@gfinnovation.com.br>
2025-07-26 14:01:02 -0300

    first commit

---
+++ src/main/java/io/github/jsousa32/libdealsign/LibDealsignApplication.java
@@ -0,0 +1,13 @@
+package io.github.jsousa32.libdealsign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibDealsignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibDealsignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/jsousa32/libdealsign/LibDealsignApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jsousa32.libdealsign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibDealsignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
