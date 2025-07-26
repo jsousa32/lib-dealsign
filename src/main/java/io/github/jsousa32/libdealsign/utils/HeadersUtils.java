1310c1568d18541e00de2f9ae561784024ac799b
João Lucas <joao.silva@gfinnovation.com.br>
2025-07-26 17:40:08 -0300

    [ADD]: adicionado o content-type e construção do http entity sem body

--- src/main/java/io/github/jsousa32/libdealsign/utils/HeadersUtils.java
+++ src/main/java/io/github/jsousa32/libdealsign/utils/HeadersUtils.java
@@ -2,6 +2,7 @@ package io.github.jsousa32.libdealsign.utils;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 public final class HeadersUtils {
 
@@ -17,7 +18,19 @@ public final class HeadersUtils {
         final var headers = new HttpHeaders();
 
         headers.add(HttpHeaders.AUTHORIZATION, BEARER.concat(aBearer));
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
         return new HttpEntity<>(body, headers);
+    }
+
+    public static HttpEntity<Object> generate(
+            final String aBearer
+    ) {
+        final var headers = new HttpHeaders();
+
+        headers.add(HttpHeaders.AUTHORIZATION, BEARER.concat(aBearer));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        return new HttpEntity<>(headers);
     }
 }
