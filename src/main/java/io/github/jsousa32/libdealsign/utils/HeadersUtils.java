85c8c54b87509032b6b17bd0ecb4aa63aeec1639
João Lucas <joao.silva@gfinnovation.com.br>
2025-07-30 21:56:14 -0300

    [ADD]: adicionado cabeçalho para o casos de form-data

--- src/main/java/io/github/jsousa32/libdealsign/utils/HeadersUtils.java
+++ src/main/java/io/github/jsousa32/libdealsign/utils/HeadersUtils.java
@@ -3,6 +3,9 @@ package io.github.jsousa32.libdealsign.utils;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+
+import java.util.List;
 
 public final class HeadersUtils {
 
@@ -32,5 +35,17 @@ public final class HeadersUtils {
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         return new HttpEntity<>(headers);
+    }
+
+    public static <T> HttpEntity<T> generateForm(
+            final String aBearer,
+            final T body
+    ) {
+        final var headers = new HttpHeaders();
+
+        headers.add(HttpHeaders.AUTHORIZATION, BEARER.concat(aBearer));
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        return new HttpEntity<>(body, headers);
     }
 }
