af9d51282eddbd0d7eea707f4a090a084499a7ee
João Lucas <joao.silva@gfinnovation.com.br>
2025-07-30 22:37:31 -0300

    [ADD]: adicionado a classe utilitária para geração do form-data

---
+++ src/main/java/io/github/jsousa32/libdealsign/utils/GenerateFormUtils.java
@@ -0,0 +1,32 @@
+package io.github.jsousa32.libdealsign.utils;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public final class GenerateFormUtils {
+
+    private GenerateFormUtils() {
+    }
+
+    public static MultipartBodyBuilder generate(final MultipartFile aFile) throws IOException {
+        String filename = null;
+
+        final var resource = new ByteArrayResource(aFile.getBytes()) {
+            @Override
+            public String getFilename() {
+                return filename;
+            }
+        };
+
+        final var builder = new MultipartBodyBuilder();
+
+        builder.part("name", Optional.ofNullable(aFile.getOriginalFilename()).orElse("Arquivo_".concat(String.valueOf(i))));
+        builder.part("file", resource);
+
+        return builder;
+    }
+}
